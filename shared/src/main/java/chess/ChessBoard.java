4747c7a5a7b00261995a1328c0d47efb7c5c8bef
Grant-Chamberlain <granttc@byu.edu>
2026-09-14 22:41:50 -0600

    add piece and getpiece are functional in chessBoard

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -8,8 +8,13 @@ package chess;
  */
 public class ChessBoard {
 
+    private ChessPiece[][] board;
+
     public ChessBoard() {
-        
+
+        board = new ChessPiece[9][9];
+
+
     }
 
     /**
@@ -19,7 +24,10 @@ public class ChessBoard {
      * @param piece    the piece to add
      */
     public void addPiece(ChessPosition position, ChessPiece piece) {
-        throw new RuntimeException("Not implemented");
+        int row = position.getRow();
+        int col = position.getColumn();
+
+        board[row][col] = piece;
     }
 
     /**
@@ -30,7 +38,10 @@ public class ChessBoard {
      * position
      */
     public ChessPiece getPiece(ChessPosition position) {
-        throw new RuntimeException("Not implemented");
+        int row = position.getRow();
+        int col = position.getColumn();
+        return board[row][col];
+
     }
 
     /**
@@ -38,6 +49,6 @@ public class ChessBoard {
      * (How the game of chess normally starts)
      */
     public void resetBoard() {
-        throw new RuntimeException("Not implemented");
+
     }
 }
